e359b34032e68e7b04db5884141d5e4ccd260f90
KrauszMartonAlajos <77116668+KrauszMartonAlajos@users.noreply.github.com>
2023-02-11 23:11:02 +0100

    Kép generáló
    még köze sincs a működőképes állapothoz pont mint nekem szombat este negyed 12-kor

--- script.js
+++ script.js
@@ -37,16 +37,52 @@ function TablaGeneralas()
         {
             var oszlopDiv = document.createElement("div");
             oszlopDiv.classList += " oszlopdiv";
-            oszlopDiv.innerHTML = "X";
+            //oszlopDiv.innerHTML = "X";
             sorDiv.appendChild(oszlopDiv);
         }
         tabla.appendChild(sorDiv);
     }
 }
+
+function RandomKepValasztas()
+{
+    var ImageArray = new Array();
+    ImageArray[0] = '1.jpg';
+    ImageArray[1] = '2.jpg';
+    ImageArray[2] = '3.jpg';
+    ImageArray[3] = '4.jpg';
+    ImageArray[4] = '5.jpg';
+    ImageArray[5] = '6.jpg';
+    ImageArray[6] = '7.jpg';
+    ImageArray[7] = '8.jpg';
+    ImageArray[8] = '9.jpg';
+    ImageArray[9] = '10.jpg';
+    ImageArray[10] = '11.jpg';
+    ImageArray[11] = '12.jpg';
+    ImageArray[12] = '13.jpg';
+    ImageArray[14] = '14.jpg';
+    ImageArray[15] = '15.jpg';
+    ImageArray[16] = '16.jpg';
+    ImageArray[17] = '17.jpg';
+    ImageArray[18] = '18.jpg';
+    ImageArray[19] = '19.jpg';
+    ImageArray[20] = '20.jpg';
+    ImageArray[21] = '21.jpg';
+    ImageArray[22] = '22.jpg';
+    ImageArray[23] = '23.jpg';
+    var rnd = Math.floor(Math.random()*ImageArray.length);
+
+    oszlopDiv.innerHTML = ImageArray[rnd];
+
+
+
+}
+
 function Main()
 {
     JatekterBetoltes();
     JatekterElrendezes();
     TablaGeneralas();
+    RandomKepValasztas();
 }
 Main();

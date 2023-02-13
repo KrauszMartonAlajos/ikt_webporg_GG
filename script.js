0ff2fa73645ac319083454d6c3d5cfcd9fe6f604
KrauszMartonAlajos <77116668+KrauszMartonAlajos@users.noreply.github.com>
2023-02-13 12:14:04 +0100

    02_13 orai feladat/1

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ var kartyaBox = document.createElement("div");
 var pontokBox = document.createElement("div");
 var tabla = document.createElement("div");
 var korokBox = document.createElement("div");
-
+// - tölts be egy képet az első cellába
+// - véletlenszerűen válassz ki egy képet és tedd az első cellába
+// - véletlen helyre helyezd el a véletlen kiválasztott képet
+// - paraméter segítségével megadott darabszámú képet helyezz el, véletlen helyre.
+//id-val kell megszámozni a cellákat
 function JatekterBetoltes()
 {
     balPanel.appendChild(kartyaBox);
@@ -29,6 +33,7 @@ function JatekterElrendezes()
 }
 function TablaGeneralas()
 {
+    var k = 1;
     for(var i = 0; i < 5; i++)
     {
         var sorDiv = document.createElement("div");
@@ -38,43 +43,25 @@ function TablaGeneralas()
             var oszlopDiv = document.createElement("div");
             oszlopDiv.classList += " oszlopdiv";
             //oszlopDiv.innerHTML = "X";
+            oszlopDiv.id = k;
             sorDiv.appendChild(oszlopDiv);
+            k++;
         }
         tabla.appendChild(sorDiv);
     }
 }
 
-function RandomKepValasztas()
+function TablaFeltoltes(db)
 {
-    var ImageArray = new Array();
-    ImageArray[0] = '1.jpg';
-    ImageArray[1] = '2.jpg';
-    ImageArray[2] = '3.jpg';
-    ImageArray[3] = '4.jpg';
-    ImageArray[4] = '5.jpg';
-    ImageArray[5] = '6.jpg';
-    ImageArray[6] = '7.jpg';
-    ImageArray[7] = '8.jpg';
-    ImageArray[8] = '9.jpg';
-    ImageArray[9] = '10.jpg';
-    ImageArray[10] = '11.jpg';
-    ImageArray[11] = '12.jpg';
-    ImageArray[12] = '13.jpg';
-    ImageArray[14] = '14.jpg';
-    ImageArray[15] = '15.jpg';
-    ImageArray[16] = '16.jpg';
-    ImageArray[17] = '17.jpg';
-    ImageArray[18] = '18.jpg';
-    ImageArray[19] = '19.jpg';
-    ImageArray[20] = '20.jpg';
-    ImageArray[21] = '21.jpg';
-    ImageArray[22] = '22.jpg';
-    ImageArray[23] = '23.jpg';
-    var rnd = Math.floor(Math.random()*ImageArray.length);
-
-    oszlopDiv.innerHTML = ImageArray[rnd];
-
-
+    console.log("kapcsolat");
+    for(var i = 0;i<db;i++)
+    {
+        var kep = document.createElement("img");
+        kep.src = "kepek/Lapok/"+Math.floor(Math.random()*23+1)+".jpg";
+        var velcella = Math.floor(Math.random()*30+1);
+        var cella = document.getElementById(velcella);
+        cella.appendChild(kep);
+    }
 
 }
 
@@ -83,6 +70,6 @@ function Main()
     JatekterBetoltes();
     JatekterElrendezes();
     TablaGeneralas();
-    RandomKepValasztas();
+    TablaFeltoltes(5);
 }
 Main();
